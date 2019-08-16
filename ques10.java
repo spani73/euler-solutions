5974c7d30b08255c30af6f211dc7514c98d25405
subhu73 <52607552+subhu73@users.noreply.github.com>
2019-08-16 13:13:21 +0530

    Add files via upload

---
+++ ques10.java
@@ -0,0 +1,31 @@
+public class NewClass
+{
+      void Primenum()
+	{
+		long i, c=2,s=0;
+		for(i=3;c<=10001;i++)
+		{
+			if(Prime(i)==1)
+			{
+				s=i;
+				c++;
+			}
+		}
+		System.out.println(s);
+	}
+   int Prime(long n)
+          {
+	        
+	        for (int i = 2; i < Math.sqrt(n) + 1; i++)
+                { if (n % i == 0)
+                       {   return 0;
+                       }
+                }
+	        return 1;
+	       }
+	public static void main(String[] args)
+	{
+		NewClass obj=new NewClass();
+                obj.Primenum();
+	}
+}
